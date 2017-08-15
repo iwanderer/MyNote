be6beeeacff406ffeab43c85602230def117f3f6
iwanderer <iwanderer.lee@gmail.com>
2017-08-15 09:55:35 +0800

    Initial commit

---
+++ app/src/main/java/com/iwanderer/mynote/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.iwanderer.mynote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+ *笔记内容Activity
+ *
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
